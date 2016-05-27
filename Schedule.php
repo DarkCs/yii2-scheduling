8682a35a8c4522737f0769771a865857e51bf798
Dark_Cs <darkcs2@gmail.com>
2016-05-27 19:50:07 +0300

    autodetect yii executable file

--- Schedule.php
+++ Schedule.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace omnilight\scheduling;
-use yii\base\Component;
+
 use yii\base\Application;
+use yii\base\Component;
 
 
 /**
@@ -10,6 +11,8 @@ use yii\base\Application;
  */
 class Schedule extends Component
 {
+    public $yiiExecutable;
+
     /**
      * All of the events on the schedule.
      *
@@ -17,33 +20,39 @@ class Schedule extends Component
      */
     protected $_events = [];
 
+    public function init()
+    {
+        $this->yiiExecutable = $this->yiiExecutable ?: basename(\Yii::$app->request->getScriptFile());
+    }
+
     /**
      * Add a new callback event to the schedule.
      *
-     * @param  string  $callback
-     * @param  array   $parameters
+     * @param  string $callback
+     * @param  array $parameters
      * @return Event
      */
-    public function call($callback, array $parameters = array())
+    public function call($callback, array $parameters = [])
     {
         $this->_events[] = $event = new CallbackEvent($callback, $parameters);
         return $event;
     }
+
     /**
      * Add a new cli command event to the schedule.
      *
-     * @param  string  $command
+     * @param  string $command
      * @return Event
      */
     public function command($command)
     {
-        return $this->exec(PHP_BINARY.' yii '.$command);
+        return $this->exec(sprintf('%s %s %s', PHP_BINARY, $this->yiiExecutable, $command));
     }
 
     /**
      * Add a new command event to the schedule.
      *
-     * @param  string  $command
+     * @param  string $command
      * @return Event
      */
     public function exec($command)
@@ -65,8 +74,7 @@ class Schedule extends Component
      */
     public function dueEvents(Application $app)
     {
-        return array_filter($this->_events, function(Event $event) use ($app)
-        {
+        return array_filter($this->_events, function (Event $event) use ($app) {
             return $event->isDue($app);
         });
     }
